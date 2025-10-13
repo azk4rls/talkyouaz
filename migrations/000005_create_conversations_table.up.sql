bb9e60581a9554e7df129a77ea117ef228ed43c0
azk4rls <m.azkarildelfiera@gmail.com>
2025-10-13 12:11:16 +0700

    Clean we

---
+++ migrations/000005_create_conversations_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE conversations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    transcript TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
